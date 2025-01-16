0612c4b1a8bc0d6528f73bded3a685a0034de60b
Yena Kim <120614713+ynkim-03@users.noreply.github.com>
2025-01-16 19:50:41 +0900

    project setup

---
+++ tests/userprog/read-normal.c
@@ -0,0 +1,7 @@
+/* Try reading a file in the most normal way. */
+
+#include "tests/lib.h"
+#include "tests/main.h"
+#include "tests/userprog/sample.inc"
+
+void test_main(void) { check_file("sample.txt", sample, sizeof sample - 1); }
