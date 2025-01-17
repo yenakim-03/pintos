7fa86d3950e3a93ef1eddc8d863d329cbadac1ad
Yena Kim <ynkim4862@gmail.com>
2025-01-17 11:30:47 +0900

    add check_priority_and_yield in sema_up to ensure priority-based thread scheduling when a new thread wakes up

--- threads/synch.c
+++ threads/synch.c
@@ -105,11 +105,12 @@ void sema_up(struct semaphore *sema) {
     ASSERT(sema != NULL);
 
     old_level = intr_disable();
-    if (!list_empty(&sema->waiters))
-        thread_unblock(
-            list_entry(list_pop_front(&sema->waiters), struct thread, elem));
+    if (!list_empty(&sema->waiters)) {
+        thread_unblock(list_entry(list_pop_front(&sema->waiters), struct thread, elem));
+    }
     sema->value++;
     intr_set_level(old_level);
+    check_priority_and_yield();
 }
 
 static void sema_test_helper(void *sema_);
